a04fc6ca700886612b9b58d0534b03ad741570e9
eminemoholic <adityaadi1467@gmail.com>
2017-02-08 00:44:37 +0530

    First release

---
+++ withoutgui.py
@@ -0,0 +1,9 @@
+import alphabeter
+
+alphabeter.setBackGroundColor("red")
+alphabeter.setForeGroundColor("orange")
+alphabeter.setTxt("this is random text")
+alphabeter.setStartX(200)
+alphabeter.setFontSize(10)
+alphabeter.setSpeed(200)
+alphabeter.printData()
